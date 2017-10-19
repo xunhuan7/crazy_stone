e52a8cf2a7ef03568b234e4900087a2c2fbc3aeb
Jason.Deng <676081195@qq.com>
2017-10-19 12:56:23 +0800

    重装系统

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,7 @@ Vue.use(ElementUI);
 
 axios.defaults.baseURL = 'http://192.168.1.103:8080/XJS/';
 axios.defaults.timeout = 10000;
+// 是否使用跨域请求
 axios.defaults.withCredentials = true;
 
 export default new Router({
